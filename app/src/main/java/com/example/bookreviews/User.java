09cb0ed9aa803babe82378192fc4fc0e1ac7562b
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-11-23 21:40:53 +0100

    LoginActivity implemented

---
+++ app/src/main/java/com/example/bookreviews/User.java
@@ -0,0 +1,39 @@
+package com.example.bookreviews;
+
+public class User {
+    private String id, name, bio, username;
+    public User(){
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
