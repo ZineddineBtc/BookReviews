a6ed0031ef6dd8e695947938c544094b9de3cbad
Zineddine Bettouche <zineddine.bettouche@stud.th-deg.de>
2020-11-24 19:04:35 +0100

    Entry updated.

--- app/src/main/java/com/example/bookreviews/activity/TermsActivity.java
+++ app/src/main/java/com/example/bookreviews/activity/TermsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.bookreviews.R;
+
 public class TermsActivity extends AppCompatActivity {
 
     @Override
